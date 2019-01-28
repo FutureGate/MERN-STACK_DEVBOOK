2146807d22e9bb836ca3f87514086b8b8597d75c
Jeong hyeon Choi <futuregate0815@gmail.com>
2019-01-28 14:19:04 +0900

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:admin1234@ds135689.mlab.com:35689/sns',
+    mongoURI: '몽고DB 커넥터 주소를 입력해주세요! (ID, PW 포함)'
+    // 예시 : mongodb://[ID]:[PW]@[MongoDB주소]/[데이터베이스 이름]'
     secretOrKey: 'secret'
 };
